7ea715e16e0409d9271de065ef40751c2277a30f
Mihir-Deshmukh <mihirdeshmukh15@gmail.com>
2024-02-12 14:34:19 -0500

    Added feature handling and fundamental matrix calculation

---
+++ Phase1/EstimateFundamentalMatrix.py
@@ -0,0 +1,99 @@
+import numpy as np
+import cv2
+
+# Reference: https://imkaywu.github.io/blog/2017/06/fundamental-matrix/
+
+def estimate_fundamental_matrix(matches):
+
+    image1_uv = np.array([match['image1_uv'] + (1,) for match in matches])
+    image2_uv = np.array([match['image2_uv'] + (1,) for match in matches])
+    # print(image1_uv)
+    
+    # Normalize the coordinates
+    image1_uv, T1 = normalize_pts(image1_uv)
+    image2_uv, T2 = normalize_pts(image2_uv)
+    
+    # print(image1_uv)
+    
+    # Estimate the fundamental matrix
+    # F, _ = cv2.findFundamentalMat(image1_uv, image2_uv, cv2.FM_RANSAC)
+    # return F
+    
+    A = np.zeros((image1_uv.shape[0],9))
+    for i in range(image1_uv.shape[0]):
+        x_1,y_1 = image1_uv[i][0], image1_uv[i][1]
+        x_2,y_2 = image2_uv[i][0], image2_uv[i][1]
+        A[i] = np.array([x_1*x_2, x_2*y_1, x_2, y_2*x_1, y_2*y_1, y_2, x_1, y_1, 1])
+    
+    
+    print(A.shape)
+    U,S,V = np.linalg.svd(A)
+    # print(S)
+    F = V.T[-1].reshape(3,3)
+
+    U,S,V = np.linalg.svd(F)
+    # print(S)
+    S[2] = 0                            #rank 2 constraint
+    F = np.dot(U,np.dot(np.diag(S),V))
+    
+    F = np.dot(T2.T, np.dot(F, T1))   #This is given in algorithm for normalization
+    F = F / F[2,2]
+    
+    return F
+
+def normalize_pts(pts):
+    pts_xy = np.array(pts)[:, :2]
+    
+    # Compute the centroid of the points
+    c = np.mean(pts_xy, axis=0)
+    newpts_xy = pts_xy - c
+    dist = np.mean(np.linalg.norm(newpts_xy, axis=1))
+    
+    scale = np.sqrt(2) / dist
+    newpts_xy_scaled = newpts_xy * scale
+    newpts = np.hstack([newpts_xy_scaled, np.ones((newpts_xy_scaled.shape[0], 1))])
+    
+    T = np.array([[scale, 0, -scale * c[0]],
+                  [0, scale, -scale * c[1]],
+                  [0, 0, 1]])
+    
+    return newpts, T
+
+
+def parse_matching(file_path, pair):
+    matches = []
+    
+    with open(file_path, 'r') as file:
+        n_features = int(file.readline().split(":")[1].strip())
+        for line in file:
+            parts = line.strip().split()
+            
+            n_matches = int(parts[0])
+            color = tuple(map(int, parts[1:4]))
+            current_image_uv = tuple(map(float, parts[4:6]))
+
+            i = 0
+            offset = 6  # Starting offset for match information
+            flag = False
+            
+            while offset < len(parts):
+                
+                if int(parts[offset]) == pair[1]:
+                    pair_uv = (float(parts[offset + 1]), float(parts[offset + 2]))
+                    flag = True
+                    break
+                    
+                
+                offset += 3  # Moving to the next match set
+                i += 1
+                if i >= n_matches:  # Break if we've read the number of matches specified for this feature
+                    break
+            
+            
+            if flag:    
+                matches.append({
+                    'color': color,
+                    'image1_uv': current_image_uv,
+                    'image2_uv': pair_uv
+                })
+    return matches
--- Phase1/Wrapper.py
+++ Phase1/Wrapper.py
@@ -4,21 +4,28 @@ import argparse
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from EstimateFundamentalMatrix import *
 
 # Read images
 def read_images(path):
     images = []
     for i in range(5):
         images.append(cv2.imread(f"{path}{i+1}.png"))
-        cv2.imshow(f"Image {i+1}", images[i])
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        # cv2.imshow(f"Image {i+1}", images[i])
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
     
     return images
 
-
-def read_features(path):
-    pass
+def read_intrinsics(path):
+    with open(path, 'r') as file:
+        lines = file.readlines()
+        intrinsics = []
+        for line in lines:
+            values = line.strip().split()
+            row = [float(value) for value in values]
+            intrinsics.append(row)
+        return np.array(intrinsics)
 
 
 
@@ -26,10 +33,24 @@ def main(args):
     
     basepath = args.path
     images = read_images(basepath)
+     
+    instrinsic_parameters = read_intrinsics(basepath + 'calibration.txt')
     
-    # Read sift features in the txt files
-    # features = read_features(basepath)
-  
+    image_pairs = [(1,2), (1,3), (1,4), (1,5), (2,3), (2,4), (2,5), (3,4), (3,5), (4,5)]
+    
+    matched_pairs = []
+    
+    for pair in image_pairs:
+        matched_pairs.append(parse_matching(basepath + f'matching{pair[0]}.txt', pair))
+        
+    print(matched_pairs[0][:2])
+    
+    # Ransac and only send the top 8 feature in the fundamental matrix
+    
+    
+    print(estimate_fundamental_matrix(matched_pairs[0][:8]))
+    
+    
     
     
     
